8776b4c45d4f1aed1b58ff26f6ad43f9b8bf1c63
Julie <juliem@isahit.com>
2019-05-24 18:26:17 +0200

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'blog-exercice';
+  posts = [
+    {
+      title:"titre 1",
+      content: "blah blah 1",
+      loveIts: 0,
+      createdAt: "10/05/2019"
+    },
+    {
+      title:"titre 2",
+      content: "blah blah 2",
+      loveIts: 0,
+      createdAt: "11/05/2019"
+    },
+    {
+      title:"titre 3",
+      content: "blah blah 3",
+      loveIts: 0,
+      createdAt: "12/05/2019"
+    },
+
+  ];
 }
---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.scss']
+})
+export class PostListComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() content: string;
+  @Input() loveIts: number;
+  @Input() createdAt: Date;  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
